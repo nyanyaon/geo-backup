f15a9c04f51b4c0509f11c138dfa7565f2a4d441
nyanyaon <nyanyaonn@gmail.com>
2025-11-08 02:09:47 +0800

    add script so i don't need effort

---
+++ Persil/upload.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Database connection details
+PG_CONN="dbname=postgres host=mandalikaproject-geotile-1rxcst-db-1 user=postgres port=54321 password=kucing123"
+
+# Table name to append into
+TABLE_NAME="bidang_tanah"
+
+# Loop recursively through all .shp files in current folder
+find . -type f -name "*.shp" | while read -r SHP_FILE; do
+    echo "Processing: $SHP_FILE"
+    ogr2ogr -append \
+        -nln "$TABLE_NAME" \
+        -nlt POLYGON \
+        -lco GEOMETRY_NAME=geom \
+        -lco FID=gid \
+        -lco PRECISION=NO \
+        "PG:$PG_CONN" \
+        "$SHP_FILE"
+done
